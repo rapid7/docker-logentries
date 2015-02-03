3a4770500cdb0059934e70bc346a1157c73b5d44
Matteo Collina <hello@matteocollina.com>
2015-02-03 16:39:06 +0100

    Refactoring to be reused as a module.

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
+#! /usr/bin/env node
 
 var tls = require('tls');
 var net = require('net');
 var eos = require('end-of-stream');
 var through = require('through2');
-var argv = require('minimist')(process.argv.slice(2), {
-  boolean: ['json'],
-  alias: {
-    'token': 't',
-    'secure': 's',
-    'json': 'j'
-  },
-  default: {
-    json: false
-  }
-});
-var token = argv.token;
-var filter = through.obj(function(obj, enc, cb) {
-  this.push(token);
-  this.push(' ');
-  this.push(JSON.stringify(obj));
-  this.push('\n');
-  cb()
-});
-var out;
+var minimist = require('minimist');
+var factory = require('docker-loghose');
 
-if (!token) {
-  console.log('Usage: docker-logentries -t TOKEN [--secure] [--json]')
-  process.exit(1)
-}
-
-var loghose = require('docker-loghose')({ json: argv.json }).pipe(filter);
-
-return start()
-
-function connect(){
+function connect(opts) {
   var stream;
-  if (argv.secure) {
-    stream = tls.connect(20000, 'api.logentries.com', onSecure)
+  if (opts.secure) {
+    stream = tls.connect(20000, 'api.logentries.com', onSecure);
   } else {
-    stream = net.createConnection(10000, 'api.logentries.com')
+    stream = net.createConnection(10000, 'api.logentries.com');
   }
 
-  function onSecure(){
+  function onSecure() {
     // let's just crash if we are not secure
     if (!stram.authorized) throw new Error('secure connection not authorized');
   }
@@ -49,15 +23,65 @@ function connect(){
   return stream;
 }
 
-function start() {
-  if (out) {
-    loghose.unpipe(out);
+function start(opts) {
+  var token = opts.token;
+  var out;
+  var filter = through.obj(function(obj, enc, cb) {
+    this.push(token);
+    this.push(' ');
+    this.push(JSON.stringify(obj));
+    this.push('\n');
+    cb()
+  });
+  var loghose = factory(opts);
+  loghose.pipe(filter);
+
+  pipe();
+
+  // destroy out if loghose is destroyed
+  eos(loghose, function() {
+    out.destroy();
+  });
+
+  return loghose;
+
+  function pipe() {
+    if (out) {
+      filter.unpipe(out);
+    }
+
+    out = connect(opts);
+
+    filter.pipe(out, { end: false });
+
+    // automatically reconnect on socket failure
+    eos(out, pipe);
+  }
+}
+
+function cli() {
+  var argv = minimist(process.argv.slice(2), {
+    boolean: ['json'],
+    alias: {
+      'token': 't',
+      'secure': 's',
+      'json': 'j'
+    },
+    default: {
+      json: false
+    }
+  });
+
+  if (!argv.token) {
+    console.log('Usage: docker-logentries -t TOKEN [--secure] [--json]');
+    process.exit(1);
   }
 
-  out = connect();
+  start(argv);
+}
 
-  loghose.pipe(out, { end: false });
+module.exports = start;
 
-  // automatically reconnect on socket failure
-  eos(out, start);
+if (require.main === module) {
+  cli();
 }
